49247508cdd42d437c918cb63eaeb01120c51dfe
Zuhair Taha <zt@wizkids.dk>
2021-07-26 22:57:20 +0200

    Add subscribe

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ declare module 'tahasoft-event-emitter' {
   class EventEmitter<T> {
     emit(args: T): void;
     add(listener: (...args: T[]) => void): void;
+    subscribe(listener: (...args: T[]) => void): void;
     addListener(listener: (...args: T[]) => void): void;
     addEventListener(listener: (...args: T[]) => void): void;
     remove(listener: (...args: T[]) => void): void;
